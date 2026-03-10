cbecc2e023cbc6644e403cef17c709d3f3aafe0a
Peter <petervsjim@gmail.com>
2026-03-10 13:38:31 +0800

    modify keymap.lua to solve the visual line issue

--- lua/basics/keymap.lua
+++ lua/basics/keymap.lua
@@ -1,14 +1,14 @@
 -- Navigate by visual lines (wrapped lines)
 if vim.g.vscode then
   local vscode = require("vscode")
-  vim.keymap.set({ "n", "v" }, "j", function()
+  vim.keymap.set("n", "j", function()
     if vim.v.count == 0 then
       vscode.action("cursorDown")
     else
       vim.cmd("normal! " .. vim.v.count .. "j")
     end
   end, { silent = true })
-  vim.keymap.set({ "n", "v" }, "k", function()
+  vim.keymap.set("n", "k", function()
     if vim.v.count == 0 then
       vscode.action("cursorUp")
     else
@@ -16,6 +16,6 @@ if vim.g.vscode then
     end
   end, { silent = true })
 else
-  vim.keymap.set({ "n", "v" }, "j", "gj", { noremap = true, silent = true })
-  vim.keymap.set({ "n", "v" }, "k", "gk", { noremap = true, silent = true })
+  vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
+  vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
 end
