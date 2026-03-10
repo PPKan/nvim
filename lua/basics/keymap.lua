b4e1ef045b26ef1c2fa99a69993d85310adc138f
Peter <petervsjim@gmail.com>
2026-03-10 15:27:44 +0800

    feat: map $ to g$ and 0 to g0 for visual line navigation

--- lua/basics/keymap.lua
+++ lua/basics/keymap.lua
@@ -15,7 +15,11 @@ if vim.g.vscode then
       vim.cmd("normal! " .. vim.v.count .. "k")
     end
   end, { silent = true })
+  vim.keymap.set("n", "$", function() vscode.action("cursorEnd") end, { silent = true })
+  vim.keymap.set("n", "0", function() vscode.action("cursorHome") end, { silent = true })
 else
   vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
   vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
+  vim.keymap.set("n", "$", "g$", { noremap = true, silent = true })
+  vim.keymap.set("n", "0", "g0", { noremap = true, silent = true })
 end
