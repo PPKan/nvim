961a7842c4975a11d1906dd32f98c52ba69e572b
Peter <petervsjim@gmail.com>
2026-04-21 11:23:02 +0800

    add clipboard functions

--- lua/basics/keymap.lua
+++ lua/basics/keymap.lua
@@ -1,3 +1,9 @@
+-- reset leader key for VSCode
+if vim.g.vscode then
+  vim.keymap.set("", "<Space>", "<Nop>")
+  vim.g.mapleader = " "
+end
+
 -- Navigate by visual lines (wrapped lines)
 if vim.g.vscode then
   local vscode = require("vscode")
@@ -23,3 +29,16 @@ else
   vim.keymap.set("n", "$", "g$", { noremap = true, silent = true })
   vim.keymap.set("n", "0", "g0", { noremap = true, silent = true })
 end
+
+-- yank to system clipboard
+if vim.g.vscode then
+    -- https://github.com/vscode-neovim/vscode-neovim/issues/298
+    vim.opt.clipboard:append("unnamedplus")
+end
+
+-- second clipboard followed by leader key
+vim.keymap.set({ "n", "x" }, "<leader>y", '"zy', { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "<leader>p", '"zp', { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "<leader>d", '"zd', { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "<leader>yy", '"zyy', { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "<leader>dd", '"zdd', { noremap = true, silent = true })
